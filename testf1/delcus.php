2190ebdce07bb1d3ec5740798087b270e7b46f9d
Ash228 <52212601+Ash228@users.noreply.github.com>
2020-12-27 17:17:32 +0530

    rolled back changes

--- testf1/delcus.php
+++ testf1/delcus.php
@@ -1,15 +1,12 @@
  <?php
 include 'dbcon.php';
 error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE ^ E_STRICT);
-
 $custid = mysqli_real_escape_string($db, $_POST['custid']);
 $sql    = "DELETE FROM customer WHERE custid='$custid'";
-
 if ($stmt = mysqli_prepare($db, $sql)) {
     mysqli_stmt_bind_param($stmt, "i",$custid);
 	$res=mysqli_stmt_execute($stmt);
-	
-	if (mysqli_stmt_affected_rows($stmt)==1){
+    if (mysqli_stmt_affected_rows($stmt)==1){
 		$msg = "Customer info along with related account and deposit is deleted";
 		echo "<script type=\"text/javascript\">alert(\"$msg\");</script>";
 		header("Refresh: 0,url=tabs10.php");
