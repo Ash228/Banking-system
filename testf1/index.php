8c1f91552710ba250d8804ea968308debafa5acb
Prateek Joshi <pjoshi22900@gmail.com>
2020-12-30 19:53:44 +0530

    Updated index and s1

--- testf1/index.php
+++ testf1/index.php
@@ -10,8 +10,8 @@
 </head>
 
 <body>
-    <div class="w3-container">
-        <div class='w3-twothird w3-xxlarge'>
+    <div class="w3-container heading">
+        <div class='w3-twothird w3-xxlarge w3-text-white'>
             <p>Banking System</p>
         </div>
         <div class='w3-bar w3-third nav w3-large'>
@@ -39,7 +39,7 @@
         </div>
     </div>
     <div class="w3-container w3-animate-opacity w3-half w3-container" id="aboutUs" style="display: none;">
-        <p class="w3-medium"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
             in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
